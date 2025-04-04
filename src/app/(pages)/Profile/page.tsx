5f6f37e23fdc22c3bef71af9be1520557c162f7d
BrockSpacek <brockspacek1@gmail.com>
2025-04-04 10:57:30 -0700

    Simple Modal Implimented

--- src/app/(pages)/Profile/page.tsx
+++ src/app/(pages)/Profile/page.tsx
@@ -8,6 +8,15 @@ import { IUserStats } from "@/lib/Interfaces";
 import Stats from "@/lib/StatEntries.json";
 import { Input } from "@/components/ui/input";
 import {
+  Dropdown,
+  DropdownItem,
+  FileInput,
+  Label,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  TextInput,
   Accordion,
   AccordionContent,
   AccordionPanel,
@@ -24,7 +33,14 @@ const page = () => {
   const [toggleStats, setToggleStats] = useState<boolean>(true);
   const [togglePosts, setTogglePosts] = useState<boolean>(false);
   const [toggleFriends, setToggleFriends] = useState<boolean>(false);
-  const [toggleModal, setToggleModal] = useState<boolean>(false);
+  const [toggleStatModal, setToggleStatModal] = useState<boolean>(false);
+  const [openAddModal, setOpenAddModal] = useState<boolean>(false);
+
+  const [edit, setEdit] = useState<boolean>(false);
+
+  const [statTitle, setStatTitle] = useState<string>("");
+  const [statDescription, setStatDescription] = useState<string>("");
+  const [statCategories, setStatCategories] = useState<string>("");
 
   const [statsItems, setStatsItems] = useState<IUserStats[]>(Stats);
 
@@ -51,8 +67,17 @@ const page = () => {
   };
 
   const handleStatModal = () => {
-    setToggleModal(!toggleModal);
-  }
+    setToggleStatModal(!toggleStatModal);
+  };
+
+  const handleStatName = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setStatTitle(e.target.value);
+
+  const handleScore = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setStatDescription(e.target.value);
+
+  const handleCategories = (categories: string) =>
+    setStatCategories(categories);
 
   return (
     <main className="flex flex-col">
@@ -107,32 +132,117 @@ const page = () => {
 
         {/* Stats Section */}
         <div className="w-full sm:w-[90%] md:w-[90%] lg:w-[85%] mx-auto mt-8 text-center py-4">
+          <Button
+            className="bg-[#FFA62B] text-2xl font-bold px-10 py-7 rounded-3xl"
+            onClick={() => setOpenAddModal(true)}
+          >
+            Add Stat +
+          </Button>
+          <Modal className="bg-[#16697A]" show={openAddModal} onClose={() => setOpenAddModal(false)}>
+            <ModalHeader>
+              {edit ? "Edit Stat Post" : "Add Stat Post"}
+            </ModalHeader>
+            <ModalBody>
+              <form className="flex max-w-md flex-col gap-4">
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="Title">Stat Name</Label>
+                  </div>
+                  <TextInput
+                    id="Stat Name"
+                    type="text"
+                    placeholder="Stat Name"
+                    required
+                    onChange={handleStatName}
+                  />
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="descrption">Score</Label>
+                  </div>
+                  <TextInput
+                    id="Score"
+                    placeholder="Score"
+                    type="text"
+                    required
+                    onChange={handleScore}
+                  />
+                </div>
+                <div>
+                  <div className="flex items-center gap-2">
+                    <Dropdown label="Categories" dismissOnClick={true}>
+                      <DropdownItem
+                        onClick={() => handleCategories("Jiu Jitsu")}
+                      >
+                        Basketball
+                      </DropdownItem>
+                      <DropdownItem onClick={() => handleCategories("Boxing")}>
+                        Boxing
+                      </DropdownItem>
+                      <DropdownItem onClick={() => handleCategories("Kung Fu")}>
+                        Baseball
+                      </DropdownItem>
+                      <DropdownItem onClick={() => handleCategories("Kung Fu")}>
+                        Cyclying
+                      </DropdownItem>
+                      <DropdownItem onClick={() => handleCategories("Kung Fu")}>
+                        Martial Arts
+                      </DropdownItem>
+                      <DropdownItem onClick={() => handleCategories("Kung Fu")}>
+                        Volleyball
+                      </DropdownItem>
+                      <DropdownItem onClick={() => handleCategories("Kung Fu")}>
+                        Football
+                      </DropdownItem>
+                      <DropdownItem onClick={() => handleCategories("Kung Fu")}>
+                        Hockey
+                      </DropdownItem>
+                      <DropdownItem onClick={() => handleCategories("Kung Fu")}>
+                        Weight Lifting
+                      </DropdownItem>
+                      <DropdownItem onClick={() => handleCategories("Kung Fu")}>
+                        Track & Field
+                      </DropdownItem>
+                      <DropdownItem onClick={() => handleCategories("Kung Fu")}>
+                        Tennis
+                      </DropdownItem>
+                      
+                    </Dropdown>
+                  </div>
+                </div>
+              </form>
+            </ModalBody>
+            <ModalFooter>
+              <Button className="bg-[#82C0CC] text-xl" onClick={() => {}}>Save</Button>
+              <Button className="bg-red-500 text-xl" onClick={() => setOpenAddModal(false)}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </Modal>
           <div className="mt-6">
             <h2 className="text-xl font-bold mb-4">{name}'s Scores</h2>
 
             <div className="grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
               {statsItems.map((item, idx) => (
-                
-                
                 <div
                   key={idx}
                   className="px-4 pb-4 rounded-lg shadow-md flex flex-col items-center text-center bg-[#82C0CC]"
                 >
                   <div className="flex justify-end w-full">
-                  <button className=" text-3xl tracking-widest font-bold text-black pb-2">
+                    <button className=" text-3xl tracking-widest font-bold text-black pb-2 cursor-pointer">
                       ...
-                  </button>
+                    </button>
                   </div>
                   <h3 className="text-xl text-black font-bold">{item.sport}</h3>
                   <p className="text-black">{item.statName}</p>
                   <p className="text-xl text-white font-bold">{item.score}</p>
                 </div>
-            
               ))}
             </div>
           </div>
 
           {/* Add Stats Section */}
+          <div></div>
         </div>
       </div>
     </main>
