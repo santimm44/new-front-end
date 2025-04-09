087a9210a6eb688f8fb9b9b77336e851a177ded0
BrockSpacek <brockspacek1@gmail.com>
2025-04-09 11:02:18 -0700

    Grid Push

--- src/app/(pages)/Profile/page.tsx
+++ src/app/(pages)/Profile/page.tsx
@@ -767,7 +767,7 @@ const page = () => {
               <div className="mt-6">
                 <h2 className="text-xl font-bold mb-4">{name}'s Friends</h2>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                   {friends.map((friend) => (
                     <div
                       key={friend.id}
